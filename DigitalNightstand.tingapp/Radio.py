5ecda8b3f3b7865b680af5144ed8170a9963cb76
Alexander Nilsen <alexander.nilsen.no@gmail.com>
2016-01-06 11:36:17 +0100

    Update Radio. Update "tingbot-python" submodule to latest
    * [Radio] Remove default "radio_channels" value set from initialization
      if none was provided in the constructor
    * [Radio] Add "self.mplayer_path" variable
    * [Radio] Add "_create_player()" method - initialize and create the
      player

--- DigitalNightstand.tingapp/Radio.py
+++ DigitalNightstand.tingapp/Radio.py
@@ -50,47 +50,20 @@ class Radio(object):
                     json.dump(data, outfile)
 
     def __init__(self, radio_channels=None, mplayer_path="D:/Personal/Downloads/Software/AudioVideo/MPlayer/MPlayer-x86_64-r37451+g531b0a3/mplayer.exe"):
-        if radio_channels == None:
-            radio_channels =  [
-                ("NRK P1", "http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_h"),
-                ("NRK P1+", "http://lyd.nrk.no/nrk_radio_p1pluss_mp3_h"),
-                ("NRK P2", "http://lyd.nrk.no/nrk_radio_p2_mp3_h"),
-                ("NRK P3", "http://lyd.nrk.no/nrk_radio_p3_mp3_h"),
-                ("NRK P13", "http://lyd.nrk.no/nrk_radio_p13_mp3_h"),
-                ("Alltid nyheter", "http://lyd.nrk.no/nrk_radio_alltid_nyheter_mp3_h"),
-                ("Alltid RR", "http://lyd.nrk.no/nrk_radio_p3_radioresepsjonen_mp3_h"),
-                ("Jazz", "http://lyd.nrk.no/nrk_radio_jazz_mp3_h"),
-                ("Klassisk", "http://lyd.nrk.no/nrk_radio_klassisk_mp3_h"),
-                ("Folkemusikk", "http://lyd.nrk.no/nrk_radio_folkemusikk_mp3_h"),
-                ("mP3", "http://lyd.nrk.no/nrk_radio_mp3_mp3_h"),
-                ("P3 Urort", "http://lyd.nrk.no/nrk_radio_p3_urort_mp3_h"),
-                ("Sport", "http://lyd.nrk.no/nrk_radio_sport_mp3_h"),
-                ("Sapmi", "http://lyd.nrk.no/nrk_radio_sami_mp3_h"),
-                ("Super", "http://lyd.nrk.no/nrk_radio_super_mp3_h"),
-                ("P1 Buskerud", "http://lyd.nrk.no/nrk_radio_p1_buskerud_mp3_h"),
-                ("P1 Finnmark", "http://lyd.nrk.no/nrk_radio_p1_finnmark_mp3_h"),
-                ("P1 Hedemark og Oppland", "http://lyd.nrk.no/nrk_radio_p1_hedmark_og_oppland_mp3_h"),
-                ("P1 Hordaland", "http://lyd.nrk.no/nrk_radio_p1_hordaland_mp3_h"),
-                ("P1 More og Romsdal", "http://lyd.nrk.no/nrk_radio_p1_more_og_romsdal_mp3_h"),
-                ("P1 Nordland", "http://lyd.nrk.no/nrk_radio_p1_nordland_mp3_h"),
-                ("P1 Oslo og Akershus", "http://lyd.nrk.no/nrk_radio_p1_ostlandssendingen_mp3_h"),
-                ("P1 Rogaland", "http://lyd.nrk.no/nrk_radio_p1_rogaland_mp3_h"),
-                ("P1 Sogn og Fjordane", "http://lyd.nrk.no/nrk_radio_p1_sogn_og_fjordane_mp3_h"),
-                ("P1 Sorlandet", "http://lyd.nrk.no/nrk_radio_p1_sorlandet_mp3_h"),
-                ("P1 Telemark", "http://lyd.nrk.no/nrk_radio_p1_telemark_mp3_h"),
-                ("P1 Troms", "http://lyd.nrk.no/nrk_radio_p1_troms_mp3_h"),
-                ("P1 Trondelag", " http://lyd.nrk.no/nrk_radio_p1_trondelag_mp3_h"),
-                ("P1 Vestfold", "http://lyd.nrk.no/nrk_radio_p1_vestfold_mp3_h"),
-                ("P1 Ostfold", "http://lyd.nrk.no/nrk_radio_p1_ostfold_mp3_h"),
-            ]
-
         self.country_code = None
         self.radio_channels = radio_channels
         self.active_channel = 0
+        self.player = None
+        self.mplayer_path = mplayer_path
+        if self.radio_channels is not None:
+            # Only create player if radio_channels is actually set to a value
+            self._create_player()
+
+    def _create_player(self):
         self.player = Player.Player(
             self.radio_channels[self.active_channel]['stream_uri'], 
             ["-ss", "60"],
-            mplayer_path)
+            self.mplayer_path)
 
     def next_channel(self):
         self.set_channel(self.active_channel + 1)
@@ -105,10 +78,15 @@ class Radio(object):
             index = len(self.radio_channels) - 1
         self.active_channel = index
         print "Currently playing: " + self.radio_channels[self.active_channel]['name']
-        self.player.load(
-            self.radio_channels[self.active_channel]['stream_uri'],
-             ["-ss", "60"]
-        )
+        if self.player is None:
+            # If player is not initialized, create the player
+            self._create_player()
+        else:
+            # Otherwise, call the "load()" method of the player
+            self.player.load(
+                self.radio_channels[self.active_channel]['stream_uri'],
+                 ["-ss", "60"]
+            )
 
     def add_channel(self, display_name, channel, switch_to=False):
         self.radio_channels.append({
