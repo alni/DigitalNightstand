ba529068805f57a9a69342ec3a6d8d1bb2fc903e
Alexander Nilsen <alexander.nilsen.no@gmail.com>
2016-04-08 07:16:59 +0200

    Only recreate the forecast if setting has changed
    * [Weather] Add "setting_is_different()" method - checks if current
      setting from key is different than from input settings
    * [Weather] Add "settings_changed" parameter to "create_forecast()" -
      whether the settings has been changed since last request and the
      request should be recreated
    * [Weather] Update "set_settings()" - force recreating and discard/
      disable the cache temporarily of the forecast request if at least
      one of the settings has changed

--- DigitalNightstand.tingapp/Weather.py
+++ DigitalNightstand.tingapp/Weather.py
@@ -40,33 +40,67 @@ class Weather(object):
         if settings is not None and "weather" in settings:
             # Only set settings if settings is actually set and "weather" exists
             # within settings
+            settings_changed = False
+            if (self.setting_is_different("latitude", settings)
+                or self.setting_is_different("longitude", settings)
+                or self.setting_is_different("units", settings)
+                or self.setting_is_different("language", settings)):
+                # Force re-creating of the forecast object if at-least one of 
+                # the settings has changed
+                settings_changed = True
             self.settings = settings["weather"]
-            self.create_forecast()
+            if settings_changed:
+                # Force re-creating of the forecast object and discard/disable
+                # the cache temporarily
+                self.create_forecast(cache_for=0, settings_changed=settings_changed)
+            else:
+                self.create_forecast()
+
+    def setting_is_different(self, key, settings):
+        """Checks if current setting from key is different than from input
+        settings.
+        """
+        return settings[key] != self.set_settings[key]
 
     def _format_time(self, hour, minute):
         """Format hour and time as 'HH:MM'"""
         return "%02d:%02d" % (hour,minute)
 
-    def create_forecast(self, cache_for=25):
-        """Create forecast from settings"""
+    def create_forecast(self, cache_for=25, settings_changed=False):
+        """Create forecast from settings
+        
+        Parameters:
+        - cache_for : minutes to cache the request for (Defaults to 25).
+                      Set to 0 or less to disable caching
+        - settings_changed : whether the settings has been changed since last 
+                             request and the request should be re-created.
+                             (Defaults to False).
+        """
         if self.settings is not None and config.FORECASTIO_API_KEY is not None:
-            # Only create forecast if settings actually set
+            # Only create forecast if settings AND the Forecast IO API Key are
+            # actually set
             if cache_for <= 0 or (
-                self.last_update == -1 or time.time() - self.last_update > cache_for * 60
-            ):
+                self.last_update == -1 
+                or time.time() - self.last_update > cache_for * 60):
                 # only update forecast if 
                 # - "cache_for" is less than or equal to "0" minutes (cache
                 #   disabled) 
                 # - OR there is more than "cache_for" minutes since last 
                 #   forecast update
                 self.last_update = time.time() # set last update to current time
-                
+
                 lat = self.settings["latitude"]
                 lon = self.settings["longitude"]
                 units = self.settings["units"]
                 lang = self.settings["language"]
                 self.is_fetching = True
-                self.forecast = forecastio.manual(self.create_url())
+                if self.forecast == None or settings_changed:
+                    # Only (re)create the forecast if it is not set OR if the
+                    # settings has been changed
+                    self.forecast = forecastio.manual(self.create_url())
+                else:
+                    # Otherwise, just update the forecast
+                    self.forecast.update()
                 self.currently = self.forecast.currently()
                 self.is_fetching = False
 
